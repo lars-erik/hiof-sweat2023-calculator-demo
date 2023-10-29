2400711c446a9d60c0ad9afc56f66ce208079c97
Lars-Erik <lea@markedspartner.no>
2023-10-29 16:08:29 +0100

    Added test for subtraction

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,7 +3,11 @@ from approvaltests import approvals
 from calculator import CalculatorProgram
 
 
-def test_calculations():
+def test_addition():
+   report = CalculatorProgram().read_transform_calculate_format()
+   approvals.verify(report)
+
+def test_subtraction():
    report = CalculatorProgram().read_transform_calculate_format()
    approvals.verify(report)
 
